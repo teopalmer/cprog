1e68fee213358252439e17f03fc1620665b6c326
Teo <Teo@Teo.local>
2019-03-26 23:10:35 +0300

    Second task added

--- lab_04_0_2/main.c
+++ lab_04_0_2/main.c
@@ -13,41 +13,52 @@ int input()
         return n;
 }
 
-float array(int n)
+int reverse(int n)
+{
+    int r1 = 0, r2 = 0;
+    while (n > 0)
+    {
+        r1 = n % 10;
+        r2 = r2 * 10 + r1;
+        n /= 10;
+    }
+    return r2;
+}
+
+int array(int n)
 {
     int x[N];
-    int ch, c = 0;
-    float sum = 0;
-    float ar = 0;
+    int ch;
     
     for (int i = 0; i < n; i++)
     {
         ch = scanf("%d", &x[i]);
+        
         if (ch != 1)
         {
             return 100;
         }
-        if (x[i] < 0)
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (x[i] > 0)
         {
-            sum += x[i];
-            c++;
+            n++;
+            for (int j = n - 1; j > i; j--)
+            {
+                x[j] = x[j - 1];
+            }
+            x[i + 1] = reverse(x[i]);
+            i++;
         }
     }
-    if (c > 0)
+    for (int i = 0; i < n; i++)
     {
-        ar = sum / c;
+        printf("%d ", x[i]);
     }
-    else
-    {
-        ar = 0;
-    }
-    return ar;
+    return 0;
 }
 
-void output(float ar)
-{
-    printf("%.6f", ar);
-}
 
 int main()
 {
@@ -64,6 +75,6 @@ int main()
         printf("Input Error");
         return 4;
     }
-    output(out);
     return 0;
 }
+
