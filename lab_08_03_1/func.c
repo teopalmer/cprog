ee6fdf0a62c0c5e2249b4444d5425f23537145be
Teo <teopalmer@ya.ru>
2019-09-20 19:11:18 +0300

    Fix n..?

--- lab_08_03_1/func.c
+++ lab_08_03_1/func.c
@@ -19,7 +19,7 @@ int read_array(float **start, float **end, int *n)
 
     if (scanf("%d", n) != 1) 
         return ERROR;
-    if (n <= 0)
+    if (*n <= 0)
         return ERROR;
 
     *start = (float*) malloc(sizeof(float)*(*n));
