9c77f7be82d36d6f48ce5705001868117ca86a14
Teo <teopalmer@ya.ru>
2019-12-06 10:13:45 +0300

    Initial files

--- lab_12_05_01/defines.h
+++ lab_12_05_01/defines.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#define N 4
+#define N 5
 
 enum error_codes
 {
--- lab_12_05_01/main_funcs.c
+++ lab_12_05_01/main_funcs.c
@@ -6,18 +6,29 @@ node_t *get_str_raw()
     int i = 0;
     int ch = 0;
     node_t *h = NULL;
-    str_t s;
+    str_t s = { 'a' };
+    ch = scanf("%c", &s[i]);
+
     do
     {
         ch = scanf("%c", &s[i]);
         i++;
 
-        if (i == 3)
+        if (i == 4)
         {
+            //puts("*");
+            printf("%s %d", s, i);
             h = push(h, s);
+            i = 0;
         }
 
-    } while (s[i] != '\0' && ch == 1);
+    } while (ch == 1 && );
+
+    if (i != 0)
+    {
+        printf("%s %d", s, i);
+        h = push(h, s);
+    }
 
     return h->head;
 }
@@ -27,7 +38,7 @@ void out()
     node_t *n = get_str_raw();
     do
     {
-        printf("%s", n->s);
+        printf("THE STRING: %s", n->s);
         n = n->next;
     } while (n != NULL);
 
