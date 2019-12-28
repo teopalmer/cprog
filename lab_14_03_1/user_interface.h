0f33457334cb254df35b3daf860b6b64decdf3c1
Teo <teopalmer@ya.ru>
2019-12-28 14:42:16 +0300

    Add initial files

---
+++ lab_14_03_1/user_interface.h
@@ -0,0 +1,11 @@
+#include "defines.h"
+#ifndef LAB_10_USER_INTERFACE_H
+int get_key(str_t key);
+void file_print_input(item_t *p, int arraysize);
+int full_input(int *array_size, item_t **p);
+int check_if_int(char c);
+int check_if_char(char c);
+int check_if_upper(char c);
+#define LAB_10_USER_INTERFACE_H
+
+#endif //LAB_10_USER_INTERFACE_H
